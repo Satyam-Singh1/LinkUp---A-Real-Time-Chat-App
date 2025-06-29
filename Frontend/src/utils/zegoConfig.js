b31ab2f2bde7e4014392299455d2b496380c5ba1
Satyam Singh <satyamrajput9956@gmail.com>
2025-06-29 16:55:42 +0530

    Updated Video call logic

--- Frontend/src/utils/zegoConfig.js
+++
@@ -1,7 +0,0 @@
-// src/utils/zegoConfig.js
-export const ZEGOCLOUD_APP_ID = 1800661921; // ✅ Replace with your App ID
-export const SERVER_SECRET = "2e3b7ab607eb8cebbcd4f7ad98865336"; // ✅ Optional, used for backend token generation
-export const ROOM_ID = "my-room-id"; // ✅ You can make this dynamic based on your app
-export const USER_ID = String(Math.floor(Math.random() * 100000)); // ✅ Can use logged-in user ID
-export const USER_NAME = "User_" + USER_ID;
-  
