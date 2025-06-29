dbcccb62262544a0d2dc4b37d8061254284d43a3
Satyam Singh <satyamrajput9956@gmail.com>
2025-06-29 22:06:01 +0530

    Resolving naming conflicts in app.jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import Navbar from "./components/Navbar";
+
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar";
 // import VideoCall from "./components/VideoCall";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
